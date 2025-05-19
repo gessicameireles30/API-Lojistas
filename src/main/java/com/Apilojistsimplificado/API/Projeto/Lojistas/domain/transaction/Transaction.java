59ca922d09ed4263f8608887f936bbbf6090b206
gessicameireles30 <gessicadasilvameireles@gmail.com>
2025-05-19 20:19:26 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/Apilojistsimplificado/API/Projeto/Lojistas/domain/transaction/Transaction.java
@@ -0,0 +1,34 @@
+package com.Apilojistsimplificado.API.Projeto.Lojistas.domain.transaction;
+
+import com.Apilojistsimplificado.API.Projeto.Lojistas.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+
+    private LocalDateTime timesTanp;
+
+}
