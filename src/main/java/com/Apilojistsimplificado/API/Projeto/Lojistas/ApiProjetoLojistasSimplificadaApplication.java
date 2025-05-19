8c140af2e2951a288742b854356bddab5948b235
gessicameireles30 <gessicadasilvameireles@gmail.com>
2025-05-19 20:17:28 -0300

    feat: initial config

---
+++ src/main/java/com/Apilojistsimplificado/API/Projeto/Lojistas/ApiProjetoLojistasSimplificadaApplication.java
@@ -0,0 +1,13 @@
+package com.Apilojistsimplificado.API.Projeto.Lojistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjetoLojistasSimplificadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjetoLojistasSimplificadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Apilojistsimplificado/API/Projeto/Lojistas/ApiProjetoLojistasSimplificadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Apilojistsimplificado.API.Projeto.Lojistas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjetoLojistasSimplificadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
