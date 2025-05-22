5aab2bb19074e574e665eb352e47d1faecb993bd
gessicameireles30 <gessicadasilvameireles@gmail.com>
2025-05-22 16:27:08 -0300

    feat: create User

--- src/main/java/com/Apilojistsimplificado/API/Projeto/Lojistas/domain/user/User.java
+++ src/main/java/com/Apilojistsimplificado/API/Projeto/Lojistas/domain/user/User.java
@@ -1,19 +1,16 @@
 package com.Apilojistsimplificado.API.Projeto.Lojistas.domain.user;
 
+import com.Apilojistsimplificado.API.Projeto.Lojistas.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.usertype.UserType;
 
 import java.math.BigDecimal;
 
-@Entity(name="users" )
-@Table(name="users" )
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity(name="users")
+@Table(name="users")
 @EqualsAndHashCode(of="id")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -34,5 +31,95 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.document = data.document();
+        this.email = data.email();
+    }
+
+    public User(){
+
+    }
+
+    public User(Long id, String firstName, String lastName, String document, String email, String password, BigDecimal balance, UserType userType) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.document = document;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.userType = userType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
 
 }
